e4cb6abf4651476f33411ce9edddae528cdbcc7b
bryan alzate <danilocha1@gmail.com>
2024-06-21 20:40:06 -0500

    tarea 1

---
+++ rickandmorty/src/assets/componentes/card.jsx
@@ -0,0 +1,14 @@
+
+const estilos = {
+    border : "solid",
+    margin
+}
+
+const card = (props)=> {
+    return(
+        <div style={estilos}>
+            <h2>{}</h2>
+        </div>
+    )
+}
+export default card
