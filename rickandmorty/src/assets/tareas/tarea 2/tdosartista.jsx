dae312c4bb80d1972177e3634caf6ec9e0d729ad
bryan alzate <danilocha1@gmail.com>
2024-06-26 06:25:16 -0500

    evidencia tarea 2

---
+++ rickandmorty/src/assets/tareas/tarea 2/tdosartista.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const titleReact = {
+  width:"33vw",
+  backgroundColor:"blue",
+  color : "offf",
+  borderRadius : "10px"
+  
+}
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="https://static.retail.autofact.cl/blog/c_img_740x370.eztl8khp8y2ai.jpg" className="auto" alt="" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 style ={titleReact}>carro de lujo famoso</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          count is {count}
+        </button>
+        <p>
+           <code>primer codigo en ract</code>
+        </p>
+      </div>
+      <p className="read-the-docs">
+        <h2>aprendiendo con react</h2>
+      </p>
+    </>
+  )
+}
+
+export default App
